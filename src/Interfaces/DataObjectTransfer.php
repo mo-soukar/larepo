a26b3b1027cd373cbefad3006690063cf77728ef
Mohammad Soukar <mohammadsoukar5@gmail.com>
2025-03-06 09:42:32 +0300

    feat(DTO):Support Setters In DTO Objects

--- src/Interfaces/DataObjectTransfer.php
+++ src/Interfaces/DataObjectTransfer.php
@@ -1,33 +1,44 @@
 <?php
 
-    namespace Soukar\Larepo\Interfaces;
+namespace Soukar\Larepo\Interfaces;
 
-    use Soukar\Larepo\Services\ClassUtilService;
+use Soukar\Larepo\Services\ClassUtilService;
+use Illuminate\Support\Str;
 
-    abstract class DataObjectTransfer
+abstract class DataObjectTransfer
+{
+    abstract static function addRules(): array;
+    abstract static function updateRules(): array;
+    public static function fromRequest(): DataObjectTransfer
     {
-        abstract static function addRules() : array;
-        abstract static function updateRules() : array;
-        public static function fromRequest() : DataObjectTransfer
-        {
-            return self::fromArray(request()->all());
-        }
-
-        public static function fromArray(array $data) : DataObjectTransfer
-        {
-            $properties = ClassUtilService::getClassProperties(static::class);
-            $object = [];
-            foreach ($data as $key=>$value){
-                if(in_array($key,$properties))
-                    $object[$key] = $value;
-            }
-
-            return new static(...$object);
-        }
-
-        public function toArray()
-        {
-            return get_object_vars($this);
-        }
-
+        return self::fromArray(request()->all());
     }
+
+    public static function fromArray(array $data): DataObjectTransfer
+    {
+        $properties = ClassUtilService::getClassProperties(static::class);
+
+        $object = new static;
+        foreach ($data as $key => $value) {
+            if (in_array($key, $properties)) {
+
+                $setterMethod = 'set' . Str::replace(' ', '', Str::headline($key));
+                // Check if the setter method exists in the object
+                if (method_exists($object, $setterMethod)) {
+                    // Call the setter method
+                    $object->{$setterMethod}($value);
+                } else {
+                    // Fallback to direct property assignment
+                    $object->{$key} = $value;
+                }
+            }
+        }
+
+        return $object;
+    }
+
+    public function toArray()
+    {
+        return get_object_vars($this);
+    }
+}
